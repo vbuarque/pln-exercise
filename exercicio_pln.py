893dd394f2e69b0aa7d7c381c0ed013aba587593
vbuarque <viniciusbuarque17@gmail.com>
2023-11-13 19:38:34 -0300

    commit

--- exercicio_pln.py
+++ exercicio_pln.py
@@ -1,6 +1,6 @@
+import pandas as pd
 import re
 import nltk
-nltk.download('rslp')
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neural_network import MLPClassifier
@@ -11,44 +11,41 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import RSLPStemmer
 
-reviews = ["Eu amo este produto", "Este é o pior produto que já comprei", "Este produto é ok", 
-           "Eu não compraria este produto novamente", "Este produto é incrível", 
-           "Eu não gostei deste produto", "Eu compraria este produto novamente", 
-           "Este produto é aceitável", "Eu gosto deste produto", "Este produto é terrível",
-           "Este produto é mediano", "Eu não recomendaria este produto", 
-           "Este produto é fantástico", "Eu odeio este produto", 
-           "Eu poderia comprar este produto novamente"]
+# Carregar os dados
+data = pd.read_csv('bd\data.csv')
 
-sentiments = ["positive", "negative", "neutral", "negative", "positive", 
-              "negative", "positive", "neutral", "positive", "negative", 
-              "neutral", "negative", "positive", "negative", "positive"]
+# Analise do'Sentence' e 'Sentiment'
+reviews = data['Sentence'].tolist()
+sentiments = data['Sentiment'].tolist()
 
 # Pré-processamento de dados
 stop_words = set(stopwords.words('portuguese'))
 stemmer = RSLPStemmer()
 
 def preprocess(review):
-    # Tokenização
+    # Tokenização e remoção de palavras irrelevantes e lematização
     words = word_tokenize(review)
-    # Remoção de palavras irrelevantes e lematização
     words = [stemmer.stem(word) for word in words if word.isalpha() and word not in stop_words]
     return words
 
 processed_reviews = [preprocess(review) for review in reviews]
 
 # Divisão dos dados em treinamento e validação
-train_reviews, validation_reviews, train_sentiments, validation_sentiments = train_test_split(processed_reviews, sentiments, test_size=0.75, stratify=sentiments)
+train_reviews = processed_reviews[:15]
+train_sentiments = sentiments[:15]
+validation_reviews = processed_reviews[15:60]
+validation_sentiments = sentiments[15:60]
 
 # Construção do modelo Word2Vec
 w2v_model = Word2Vec(train_reviews, vector_size=100, window=5, min_count=1, workers=4)
 w2v_model.train(train_reviews, total_examples=w2v_model.corpus_count, epochs=10)
 
 # Transformação dos dados de treinamento e validação em vetores usando o modelo Word2Vec
-train_vectors = np.array([np.mean([w2v_model.wv[word] for word in review if word in w2v_model.wv.key_to_index], axis=0) for review in train_reviews])
-validation_vectors = np.array([np.mean([w2v_model.wv[word] for word in review if word in w2v_model.wv.key_to_index], axis=0) for review in validation_reviews])
+train_vectors = np.array([np.mean([w2v_model.wv[word] for word in review if word in w2v_model.wv.key_to_index], axis=0) if np.sum([word in w2v_model.wv.key_to_index for word in review]) > 0 else np.zeros(w2v_model.vector_size) for review in train_reviews])
+validation_vectors = np.array([np.mean([w2v_model.wv[word] for word in review if word in w2v_model.wv.key_to_index], axis=0) if np.sum([word in w2v_model.wv.key_to_index for word in review]) > 0 else np.zeros(w2v_model.vector_size) for review in validation_reviews])
 
-# Classificação usando MLP
-mlp = MLPClassifier(max_iter=1000, learning_rate_init=0.001)
+# Classificação usando MLP para Word2Vec
+mlp = MLPClassifier(max_iter=2000, learning_rate_init=0.001)
 mlp.fit(train_vectors, train_sentiments)
 w2v_predictions = mlp.predict(validation_vectors)
 
@@ -57,29 +54,24 @@ vectorizer = TfidfVectorizer()
 train_vectors = vectorizer.fit_transform([' '.join(review) for review in train_reviews])
 validation_vectors = vectorizer.transform([' '.join(review) for review in validation_reviews])
 
-# Classificação usando MLP
-mlp = MLPClassifier(max_iter=1000, learning_rate_init=0.001)
+# Classificação usando MLP para Bag of Words
+mlp = MLPClassifier(max_iter=2000, learning_rate_init=0.001)
 mlp.fit(train_vectors, train_sentiments)
 bow_predictions = mlp.predict(validation_vectors)
 
 # Comparação dos modelos
-print(f'Acurácia do Word2Vec: {accuracy_score(validation_sentiments, w2v_predictions)}')
-print(f'Acurácia do Bag of Words: {accuracy_score(validation_sentiments, bow_predictions)}')
+w2v_accuracy = accuracy_score(validation_sentiments, w2v_predictions)
+bow_accuracy = accuracy_score(validation_sentiments, bow_predictions)
 
-# Aplicação baseada em Word2Vec para reescrita de frases
-def rewrite_sentence(sentence):
-    words = preprocess(sentence)
-    new_words = []
-    for word in words:
-        # Encontre a palavra mais similar no modelo Word2Vec
-        try:
-            similar_words = w2v_model.wv.most_similar(word)
-            # Substitua a palavra pela palavra mais similar
-            new_words.append(similar_words[0][0])
-        except KeyError:
-            new_words.append(word)
-    return ' '.join(new_words)
+w2v_prob_mean = np.mean(w2v_predictions == validation_sentiments)
+bow_prob_mean = np.mean(bow_predictions == validation_sentiments)
 
-sentence = "Eu adoro este produto"
-new_sentence = rewrite_sentence(sentence)
-print(new_sentence)
+# Tabela comparativa
+data = {
+    'Modelo': ['Word2Vec (W2V)', 'Bag of Words (TFIDF)'],
+    'Percentual de Acerto': [w2v_accuracy * 100, bow_accuracy * 100],
+    'Probabilidade Média de Acertos': [w2v_prob_mean * 100, bow_prob_mean * 100 ]
+}
+
+df = pd.DataFrame(data)
+print(df)
